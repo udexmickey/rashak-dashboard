5940bc169c7836dda403a2ba2d2bad5a2af65eab
udexmickey <jerrymike80@yahoo.com>
2023-12-11 10:56:40 +0100

    update authApi.ts

--- src/utils/api/authApi.ts
+++ src/utils/api/authApi.ts
@@ -6,7 +6,7 @@ import axios from 'axios';
 const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
 const authApi = axios.create({
-  baseURL: BASE_URL,
+  baseURL: `${BASE_URL}/api/v1`,
   headers: {
     'Content-Type': 'application/json',
   },
